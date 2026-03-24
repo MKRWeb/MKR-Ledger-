129a4811731007e2db970bdf69c288a9116758ea
MKR Web <mkrweb2030@gmail.com>
2026-03-24 22:56:50 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Minimal Service Worker for Offline PWA Support
+const CACHE_NAME = 'mkr-ledger-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
